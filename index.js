5ef4e7962e1edf6db42a9042f6bb99eed9373a9d
Farhan <ullah8113@gmail.com>
2025-01-17 23:16:49 +0600

    Route and controller code added

--- index.js
+++ index.js
@@ -1,105 +1,39 @@
-const express = require("express");
-const cors = require("cors");
-const Post = require("./models/Post/Post");
-const connectDB = require("./utils/connectDB");
 require("dotenv").config();
-// const dotenv = require("dotenv");
-// dotenv.config();
+const corse = require("cors");
+const express = require("express");
+const connectDB = require("./utils/connectDB");
+const postRouter = require("./router/post/postsRouter");
 //call the db
 connectDB();
-
 const app = express();
+//! PORT
+const PORT = 3000;
 
-//!PORT
-const PORT = process.env.PORT || 3000;
-
-//Middleware
-app.use(express.json()); //     req.body pass json data
-//cors middleware
-app.use(cors()); //     req.headers
+//Middlewares
+app.use(express.json()); //Pass json data
+// corse middleware
 const corsOptions = {
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:5173"],
   credentials: true,
 };
-app.use(cors(corsOptions));
-//!Create Post
-app.post("/api/v1/posts/create", async (req, res) => {
-  try {
-    //get the payload
-    const postData = req.body;
-    console.log(req.body);
-    const postCreated = await Post.create(postData);
-    res.status(200).json({
-      status: "success",
-      message: "Post created successfully",
-      postCreated,
-    });
-  } catch (error) {
-    console.log(error);
-    res.json(error);
-  }
-});
-//!List Post
-app.get("/api/v1/posts", async (req, res) => {
-  try {
-    const posts = await Post.find();
-    res.json({
-      status: "success",
-      message: "Post Fetched Successfully",
-      posts,
-    });
-  } catch (error) {
-    res.json(error);
-  }
-});
-//!Update Post
-app.put("/api/v1/posts/:postId", async (req, res) => {
-  try {
-    //get the post id from params
-    const postId = req.params;
-    //find the post
-    const postFound = await Post.findById(postId);
-    if (!postFound) {
-      throw new Error("Post not found");
-    }
-    //update the post
-    const postUpdated = await Post.findByIdAndUpdate(
-      postId,
-      { title: req.body.title },
-      { description: req.body.description },
-      {
-        new: true,
-      }
-    );
-    res.json({
-      status: "success",
-      message: "Post Updated Successfully",
-      postUpdated,
-    });
-  } catch (error) {
-    res.json(error);
-  }
-});
-//!get Post by id
-app.get("/api/v1/posts/:postId", async (req, res) => {
-  try {
-    //get the post id from params
-    const postId = req.params;
-    //find the post
-    const postFound = await Post.findById(postId);
+app.use(corse(corsOptions));
+//!---Route handlers
+app.use("/api/v1", postRouter);
 
-    res.json({
-      status: "success",
-      message: "Post Fetched Successfully",
-      postFound,
-    });
-  } catch (error) {
-    res.json(error);
-  }
+//!Not found
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Route not found on our server" });
 });
-//!Delete Post
+//! Error handling middleware
+app.use((err, req, res, next) => {
+  //prepare the error message
+  const message = err.message;
+  const stack = err.stack;
+  res.status(500).json({
+    message,
+    stack,
+  });
+});
 
-//!Start
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+//!Start the server
+app.listen(PORT, console.log(`Server is up and running on port ${PORT}`));
